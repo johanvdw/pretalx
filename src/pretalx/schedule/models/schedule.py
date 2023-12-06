6988a9378917b72034329e271f025e6695459d42
Johan Van de Wauw <johanvdw@fosdem.org>
2023-12-07 00:06:23 +0100

    Skip queue for schedule updates
    Schedule updates are triggered automatically - we should not
    require confirmation for them.
    
    We do save them though.

--- src/pretalx/schedule/models/schedule.py
+++ src/pretalx/schedule/models/schedule.py
@@ -606,6 +606,7 @@ class Schedule(PretalxModel):
                     context_kwargs={"user": speaker},
                     context={"notifications": notifications},
                     commit=save,
+                    skip_queue=True,
                     locale=locale,
                     attachments=[
                         {
