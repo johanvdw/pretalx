b09d7c911006bf582f05f2c99e3e5873039bafd1
Johan Van de Wauw <johanvdw@fosdem.org>
2024-12-14 10:05:29 +0100

    Ensure state change happens if there is no track/tracksettings
    for stands confirmation

--- src/pretalx/submission/models/submission.py
+++ src/pretalx/submission/models/submission.py
@@ -384,16 +384,17 @@ class Submission(GenerateCode, PretalxModel):
 
         if force or new_state in valid_next_states:
             # FOSDEM specific change - requires tracksetting to be set on the track
-            # (it is on all our tracks)
+            # note that stands and devrooms have no tracksetting, nor track
+            # so we skip the test in that case
+            if self.track and hasattr(self.track, 'tracksettings') :
+                if person not in self.speakers.all() and person not in self.track.tracksettings.manager_team.members.all() and not person.is_administrator:
+                    raise SubmissionError(
+                            "State can only be changed by submitter (confirm/withdraw) or track/devroom manager")
 
-            if person not in self.speakers.all() and person not in self.track.tracksettings.manager_team.members.all() and not person.is_administrator:
-                raise SubmissionError(
-                        "State can only be changed by submitter (confirm/withdraw) or track/devroom manager")
-
-            # extra check to make sure devroom manager can not accept his own talk in a different track
-            if person not in self.track.tracksettings.manager_team.members.all() and new_state==SubmissionStates.ACCEPTED and not person.is_administrator:
-                raise SubmissionError(
-                        "Talks can only be accepted by track/devroom manager or admin.")
+                # extra check to make sure devroom manager can not accept his own talk in a different track
+                if person not in self.track.tracksettings.manager_team.members.all() and new_state==SubmissionStates.ACCEPTED and not person.is_administrator:
+                    raise SubmissionError(
+                            "Talks can only be accepted by track/devroom manager or admin.")
 
             old_state = self.state
             self.state = new_state
